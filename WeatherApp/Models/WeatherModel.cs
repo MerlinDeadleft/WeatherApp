d65d31b2d195de6a55c440038b5b0fc5891ad67f
MerlinDeadleft <34375235+MerlinDeadleft@users.noreply.github.com>
2026-08-09 22:47:55 +0200

    Add basic weather model

---
+++ WeatherApp/Models/WeatherModel.cs
@@ -0,0 +1,8 @@
+namespace WeatherApp.Models;
+
+public class WeatherModel
+{
+    public string CityName { get; set; } = "";
+    public float Temperature { get; set; }
+    public string Condition { get; set; } = "";
+}
